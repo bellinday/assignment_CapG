47e9bc113f4d0f2f74bed69c9fe86d5f55069c83
Belinda Benjamin <71557073+bellinday@users.noreply.github.com>
2022-04-21 17:08:37 +0530

    Create q46.java

---
+++ q46.java
@@ -0,0 +1,63 @@
+
+package doselect_46;
+
+public class Register {
+	public String checkCredentials(String email, String pass, String cpass) throws Exception{
+		if((!email.contains("@"))||(!email.contains(".")))
+			throw new InvalidEmailException("Invalid Email");
+		else if(pass.length()<6)
+			throw new InvalidPasswordException("Invalid Password");
+		else if(cpass!=pass)
+			throw new PasswordNotMatchException("Password not match");
+		else
+			return "Registered";
+		
+	}
+	
+}
+package doselect_46;
+
+public class InvalidEmailException extends Exception {
+	public InvalidEmailException(String msg) {
+		super(msg);
+		
+	}
+
+}
+package doselect_46;
+
+public class InvalidPasswordException extends Exception {
+	public InvalidPasswordException(String msg) {
+		super(msg);
+		
+	}
+
+}
+package doselect_46;
+
+public class PasswordNotMatchException extends Exception{
+	public PasswordNotMatchException(String msg) {
+		super(msg);
+		
+	}
+
+}
+
+package doselect_46;
+
+public class main {
+
+	public static void main(String[] args) throws Exception{
+		// TODO Auto-generated method stub
+		Register user = new Register();
+		try {
+		String out = user.checkCredentials("tushar@gmailcom","hiiiiii","hiiiiii");
+		System.out.println(out);
+		}
+		catch(Exception e1) {
+		System.out.println(e1);
+		}
+
+	}
+}
+
